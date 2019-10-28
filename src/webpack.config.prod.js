97483bee3b0dacca8e9a8d673c85bb5aad3e7bb7
Zoran Markovic <zoran.markovic@netcentric.biz>
2019-10-28 23:14:25 +0100

    configure production build

---
+++ src/webpack.config.prod.js
@@ -0,0 +1,59 @@
+// this is node.js
+const path = require('path'); // provided automatically for constructing absolute path
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        // path.resolve(__dirname)  is absolute path to this file
+        // 'dist' will be the folder where bundle is generated
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    devtool: 'cheap-module-eval-source-map', // generate source maps
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.css$/,
+                exclude: '/node_modules/',
+                use: [
+                    { loader: 'style-loader' }, // injecting css code
+                    { loader: 'css-loader', // resolve imports
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base64:5]' // name of generated CSS classes in the DOM
+                            }
+                        }
+                    },
+                    { loader: 'postcss-loader', // prefix css for other browsers
+                        options: {
+                            indent: 'postcss',
+                            plugins: () => [autoprefixer()]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpg?g|gif)$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // will copy this in src/build file like other bundles
+            template: __dirname + '/src/index.html', //starting point
+            filename: 'index.html',
+            inject: 'body',
+
+        })
+    ]
+};
